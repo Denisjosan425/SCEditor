a307b634ab2316201486e461849112b4f2b22c29
Dmitry Efremov <juliusvonevola@gmail.com>
2022-07-16 16:04:54 +0300

    Add setpopulation command

--- core/server/plugins/basic_systems.lua
+++ core/server/plugins/basic_systems.lua
@@ -12,9 +12,26 @@ local function set_year(client, args)
     end
 end
 
+local function set_population(client, args)
+    local p = api.call_function("get_selected_province", client)
+    local count = tonumber(args[2])
+
+    if p and not game_data.provinces[p].w then
+        if count then
+            game_data.provinces[p].p = count
+            api.call_function("chat_message", "Province "..p.." population: "..count, "system")
+        else
+            api.call_function("chat_message", "Bad argument. /setpopulation [count]", "error", true, client)
+        end
+    else
+        api.call_function("chat_message", "Please, select province", "error", true, client)
+    end
+end
+
 function M.init(_api)
     api = _api
     api.register_command("/setyear", set_year)
+    api.register_command("/setpopulation", set_population)
 end
 
 return M
