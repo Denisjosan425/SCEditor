ad1604fb303ddcb620cbbd9e45002a08c32eb606
Dmitry Efremov <juliusvonevola@gmail.com>
2022-07-16 12:35:54 +0300

    Delete extra comments

--- server_settings.lua
+++ server_settings.lua
@@ -2,30 +2,18 @@ local t = {
 	server_info = {
 		type = "server_info",
 		data = {
-			--Server name
-			name = "Cold Path Scenario Editor",
-			--Will be automatically changed
+			name = "SCEditor v0.1",
 			players = 1,
-			--Will be automatically filled
 			server_ip = "",
-			--Port through which the server will be available
-			server_port = 2222,
-			--Will be automatically changed
+			server_port = 4000,
 			size = 10
 		}
 	},
-	--[[ This is the first number of the game version.
-	Example: game version is 5.3, server version is 5, because
-	client-server compatibility is determined by this number.
-	Do not touch in order to allow players to join. Update the server.--]]
 	SERVER_VERSION = 17,
-	-- Maximum amount of time per turn. Seconds
-	time_to_turn = 90000,
+	time_to_turn = 300,
 	verify_uuid = true,
 	plugin = {
-		welcome = [[CScenarios Tool v1.0 by Verden Brammts#6960
-		help: /h]],
-		difficulty = "standard"
+		difficulty = "easy"
 	}
 }
 return t
